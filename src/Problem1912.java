4a82632f9bc409d9b0ce2f64d38adf9ca96063c9
yangwoohyeon <dngus0717@gmail.com>
2024-05-25 21:58:56 +0900

    1912

--- src/Problem1912.java
+++ src/Problem1912.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
-public class Problem1912 {
+public class Problem1912 {//백준 1912
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
