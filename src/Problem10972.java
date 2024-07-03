bd9ac1c8b2ff953092df8b5b178cf8c4848c2d9c
yangwoohyeon <dngus0717@gmail.com>
2024-07-03 20:57:56 +0900

    백준 알고리즘

---
+++ src/Problem10972.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Problem10972 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N= scanner.nextInt();
+        int[] arr = new int[N];
+        int count = N-1;
+        int j=0;
+        for(int i=0; i<N; i++){
+            arr[i]=scanner.nextInt();
+        }
+
+        for(int i=N-1; i>0; i--){
+            if(arr[i]<arr[i-1]){
+                count--;
+            }
+            else{
+                break;
+            }
+        }
+
+        if(count==0){
+            System.out.println("-1");
+        }
+        else{
+           for(int i=count; i<N; i++){
+               if(arr[count-1]<arr[i]){
+                   j=i;
+               }
+           }
+           int tmp=arr[j];
+           arr[j]=arr[count-1];
+           arr[count-1]=tmp;
+
+           j=N-1;
+           while(count<j){
+               tmp=arr[count];
+               arr[count]=arr[j];
+               arr[j]=tmp;
+               count+=1;
+               j-=1;
+           }
+
+            for(int i=0; i<N; i++){
+                System.out.print(arr[i]+" ");;
+            }
+        }
+
+
+    }
+}
--- src/Problem15652.java
+++ src/Problem15652.java
@@ -1,5 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Problem15652 {
-    public static void main(String[] args) {
+    static StringBuilder sb = new StringBuilder();
+    static int n, m;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+        dfs(1,0);
+        System.out.println(sb);
+    }
+
+    private static void dfs(int at, int depth) {
+        if (depth == m) {
+            for (int val : arr) {
+                sb.append(val).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=at; i<=n; i++){
+                arr[depth] = i;
+                dfs(i, depth + 1);
+
+        }
 
     }
 }
