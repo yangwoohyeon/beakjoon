0b90ed93bca26555fcdf7d2c827cb44a6a98f692
yangwoohyeon <dngus0717@gmail.com>
2024-07-03 19:15:24 +0900

    백준 알고리즘

---
+++ src/Problem15651.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Problem15651 {
+    static StringBuilder sb = new StringBuilder();
+    static int n, m;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+        dfs(1,0);
+        System.out.println(sb);
+    }
+
+    private static void dfs(int at, int depth) {
+        if (depth == m) {
+            for (int val : arr) {
+                sb.append(val).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=1; i<=n; i++){
+            arr[depth]=i;
+            dfs(i+1,depth+1);
+        }
+
+    }
+}
---
+++ src/Problem15652.java
@@ -0,0 +1,5 @@
+public class Problem15652 {
+    public static void main(String[] args) {
+
+    }
+}
