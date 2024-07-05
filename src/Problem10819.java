0941e5ca8085be611f1b36e62efceadeb12b2da7
yangwoohyeon <dngus0717@gmail.com>
2024-07-05 21:25:03 +0900

    백준 알고리즘

---
+++ src/Problem10819.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Problem10819 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int [] arr = new int[N];
+        for(int i=0; i<N; i++){
+            arr[i]=scanner.nextInt();
+        }
+
+    }
+}
