423b03f12dfbba561f0a8bd1e33ccd508fc0ebbb
yangwoohyeon <dngus0717@gmail.com>
2024-07-06 19:31:10 +0900

    백준 알고리즘

---
+++ src/Problem6603.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Problem6603 {
+    static int[] selected,arr;
+    static boolean[] visited;
+    static int N;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            N=scanner.nextInt();
+            if(N==0){
+                break;
+            }
+            arr=new int[N];
+            visited = new boolean[N];
+            selected = new int[N];
+
+            for(int i=0; i<N; i++){
+                arr[i]=scanner.nextInt();
+            }
+
+            dfs(0,0);
+            System.out.println();
+
+        }
+
+    }
+    public static void dfs(int count,int start){
+        if(count==6){
+            for(int i=0; i<6; i++){
+                System.out.print(selected[i]+" ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int i=start; i<N; i++){
+            if(!visited[i]){
+                visited[i]=true;
+                selected[count]=arr[i];
+                dfs(count+1,i+1);
+                visited[i]=false;
+            }
+        }
+    }
+}
