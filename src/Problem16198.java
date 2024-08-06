e00ee328a4dac49fdd344025e752d9e85390ea71
yangwoohyeon <dngus0717@gmail.com>
2024-08-06 16:56:54 +0900

    에너지 모으기

--- src/Problem16198.java
+++ src/Problem16198.java
@@ -1,16 +1,35 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Problem16198 {
-    static int[] arr;
+    static ArrayList<Integer> list=new ArrayList<>();
     static int N;
-    static int sum=0;
+    static int Max=Integer.MIN_VALUE;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        arr=new int[N];
+        N = scanner.nextInt();
         for(int i=0; i<N; i++){
-            arr[i]=scanner.nextInt();
+            int n=scanner.nextInt();
+           list.add(n);
         }
+
+        dfs(0);
+        System.out.println(Max);
+
+    }
+    static void dfs(int value){
+        if(list.size()==2){
+            if(value>Max){
+                Max=value;
+            }
+            return;
+        }
+       for(int i=1; i<list.size()-1; i++){
+           int energy=(list.get(i-1)*list.get(i+1));
+           int n=list.remove(i);
+           dfs(value+energy);
+           list.add(i,n);
+       }
     }
 }
