c4e6fa9bd044771495a05b0dff50c257c66c8cac
yangwoohyeon <dngus0717@gmail.com>
2024-09-01 20:28:42 +0900

    ATM

--- src/Problem1080.java
+++ src/Problem1080.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Problem1080 { //
+public class Problem1080 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
