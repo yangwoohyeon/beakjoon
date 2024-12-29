6900c7e9947d7fcd1007f5f9e8ff606de0dc083f
yangwoohyeon <dngus0717@gmail.com>
2024-12-29 15:14:54 +0900

    하노이 탑 이동 순서

---
+++ src/Problem11729.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Problem11729 {
+    static int N;
+    public static  StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        N = scanner.nextInt();
+
+
+        sb.append((int)Math.pow(2,N)-1).append('\n');
+        move(N,1,2,3);
+        System.out.println(sb);
+    }
+
+    public static void move(int n,int h1,int h2, int h3) {
+            if(n==1){
+            sb.append(h1+" "+h3+"\n");
+            return;
+        }
+        move(n-1,h1,h3,h2);
+        sb.append(h1+" "+h3+"\n");
+        move(n-1,h2,h1,h3);
+    }
+
+}
