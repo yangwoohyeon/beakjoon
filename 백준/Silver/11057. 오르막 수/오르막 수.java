d3c1f164e7a0c093aded89db1efa09522dd7c864
yangwoohyeon <dngus0717@gmail.com>
2025-08-13 20:16:57 +0900

    [Silver I] Title: 오르막 수, Time: 176 ms, Memory: 17836 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.java
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -1,32 +1,28 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Scanner;
+    import java.util.Scanner;
 
-public class Main {
-	
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int n=scanner.nextInt();
-		
-		long[][] dp = new long[n+1][10];
-		
-		for(int i=0; i<10; i++) {
-			dp[1][i]=1;
-		}
-		
-		for(int i=2; i<=n; i++) {
-			for(int j=0; j<=9; j++) {
-				for(int k=j; k<=9; k++) {
-				dp[i][j]=(dp[i][j]+dp[i-1][k])%10007;
-				}
-			}
-		}
-		long sum=0;
-		for(int i=0; i<10; i++) {
-			sum=(sum+dp[n][i])%10007;
-		}
-		
-		System.out.println(sum);
-	}
-}
+    public class Main {
+        static final int MOD = 10007;
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            long[][] dp = new long[1001][10];
+
+            int N = scanner.nextInt();
+
+            for(int i=0; i<N; i++){
+                for(int j=0; j<=9; j++){
+                    if(j==0 || i==0){
+                        dp[i][j]=1;
+                    }
+                    else{
+                        dp[i][j]=(dp[i][j-1]+dp[i-1][j])%MOD;
+                    }
+                }
+            }
+            int sum=0;
+            for(int i=0; i<10; i++){
+                sum+=dp[N-1][i];
+            }
+            System.out.println(sum%MOD);
+        }
+    }
