7cbf2e16215cb2be81f4326da5e764299b547321
yangwoohyeon <dngus0717@gmail.com>
2025-10-19 11:43:29 +0900

    [level 2] Title: 삼각 달팽이, Time: 19.98 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,63 +1,78 @@
 import java.util.*;
-
 class Solution {
+    int[][] triangle;
+    int size=0;
     public int[] solution(int n) {
-        int[][] triangle = new int[n][n];
-        int v=1;
-        int x=0;
-        int y=0;
-        while(true){
-        //아래로 이동
-        while(true){
-            triangle[y][x] = v++;
-            if(y+1 == n || triangle[y+1][x]!=0){
-                break;
-            }
-            y+=1;
+        int[] answer;
+        if(n==1){
+            return new int[]{1};
         }
-        if(x+1 == n || triangle[y][x+1]!=0){
-            break;
-        }
-        x+=1;
+        triangle = new int[n][n];
+        func(n);
+        answer = new int[size];
+        int idx=0;
         
-        //오른쪽으로 이동
-        while(true){
-            triangle[y][x] = v++;
-            if(x+1 == n || triangle[y][x+1]!=0){
-                break;
-            }
-            x+=1;
-        }
-        
-        if(triangle[y-1][x-1]!=0){
-            break;
-        }
-        x-=1;
-        y-=1;
-        //왼쪽 위로 이동
-        while(true){
-            triangle[y][x]=v++;
-            if(triangle[y-1][x-1]!=0){
-                break;
-            }
-            x-=1;
-            y-=1;
-        }
-        if(y+1 == n || triangle[y+1][x] != 0){
-            break;
-        }
-            y+=1;
-    }
-        int[] result = new int[v-1];
-        int index=0;
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                if(triangle[i][j]!=0){
-                    result[index++]=triangle[i][j];
+                if(triangle[i][j]==0){
+                    j=n;
+                }
+                else{
+                    answer[idx++]=triangle[i][j];
                 }
             }
         }
-        return result;
+        return answer;
     }
-    
+    private void func(int n){
+        String state="down";
+        int repeat = n;
+        int x = -1;
+        int y = 0;
+        int number = 1;
+        while(true){
+            if(state.equals("down")){
+                if(x+1<n && triangle[x+1][y]!=0){
+                    return;
+                }
+                for(int i=0; i<repeat; i++){
+                        x++;
+                        triangle[x][y]=number++;
+                        size++;
+                }
+                state="right";
+                repeat--;
+            }
+            
+            if(state.equals("right")){
+                if(y+1<n && triangle[x][y+1]!=0){
+                    return;
+                }
+                for(int i=0; i<repeat; i++){
+                        y++;
+                        triangle[x][y]=number++;
+                    
+                        size++;
+                    
+                }
+                state="up";
+                repeat--;
+            }
+            
+            if(state.equals("up")){
+                if(x-1>=0 && y-1>=0 && triangle[x-1][y-1]!=0){
+                    return;
+                }
+                for(int i=0; i<repeat; i++){
+                        y--;
+                        x--;
+                        triangle[x][y]=number++;
+                        size++;
+                
+                }
+                state="down";
+                repeat--;
+            }
+        }
+    }
 }
