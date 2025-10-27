f2c4f6b537e3d8c80374c8c9ef3328517cbffa6f
yangwoohyeon <dngus0717@gmail.com>
2025-10-27 12:19:27 +0900

    [level 2] Title: 올바른 괄호, Time: 17.40 ms, Memory: 54.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,25 +2,24 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-        Stack<Character> stack = new Stack<>();
+      Stack<Character> stack = new Stack<>();
         
         for(int i=0; i<s.length(); i++){
-            if(s.charAt(i)=='('){
-                stack.add(s.charAt(i));
+            char c = s.charAt(i);
+            if(c=='('){
+                stack.push(c);
             }
             else{
                 if(stack.isEmpty()){
                     return false;
                 }
                 stack.pop();
-                
             }
         }
+
         if(!stack.isEmpty()){
             return false;
         }
-
-        return answer;
+        return true;
     }
 }
