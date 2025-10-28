ae2d28e551e43906bb10858f4f6fe647a88ddea9
yangwoohyeon <dngus0717@gmail.com>
2025-10-28 14:54:32 +0900

    [Silver I] Title: 트리 순회, Time: 172 ms, Memory: 17632 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -1,72 +1,73 @@
-    import java.util.Scanner;
+import java.util.*;
+public class Main {
+    public static Node head = new Node('A',null,null);
+    static class Node{
+        char data;
+        Node left;
+        Node right;
 
-    public class Main {
-        static Node head = new Node('A',null,null);
-        static class Node{
-            char data;
-            Node left;
-            Node right;
-            public Node(char data, Node left, Node right){
-                this.data = data;
-                this.left = left;
-                this.right = right;
-            }
-
-
+        public Node(char data, Node left, Node right){
+            this.data = data;
+            this.left = left;
+            this.right = right;
         }
-        public static void insertNode(Node tmp, char root, char left, char right){
-            if(tmp.data == root){
-                tmp.left = (left == '.' ? null : new Node(left,null,null));
-                tmp.right = (right =='.'? null : new Node(right,null,null));
+    }
+    public static void insertNode(Node tmp, char root, char left, char right){
+        if(tmp.data == root){
+            if(left!='.'){
+                tmp.left=new Node(left,null,null);
             }
-            else {
-                if (tmp.left != null) {
-                    insertNode(tmp.left, root, left, right);
-                }
-                if (tmp.right != null) {
-                    insertNode(tmp.right, root, left, right);
-                }
+            if(right!='.'){
+                tmp.right=new Node(right,null,null);
             }
         }
-
-        public static void pre(Node head){
-            if(head == null){
-                return;
+        else{
+            if(tmp.left!=null){
+                insertNode(tmp.left,root,left,right);
             }
-            System.out.print(head.data);
-            pre(head.left);
-            pre(head.right);
-        }
-        public static void in(Node head){
-            if(head == null){
-                return;
+            if(tmp.right!=null){
+                insertNode(tmp.right,root,left,right);
             }
-            in(head.left);
-            System.out.print(head.data);
-            in(head.right);
-        }
-        public static void post(Node head){
-            if(head == null){
-                return;
-            }
-            post(head.left);
-            post(head.right);
-            System.out.print(head.data);
-        }
-        public static void main(String[] args) {
-            int n;
-            Scanner sc = new Scanner(System.in);
-            n=sc.nextInt();
-            sc.nextLine();
-            for(int i=0; i<n; i++){
-                String s = sc.nextLine();
-                insertNode(head,s.charAt(0),s.charAt(2),s.charAt(4));
-            }
-            pre(head);
-            System.out.println();
-            in(head);
-            System.out.println();
-            post(head);
         }
 
     }
+    public static void pre(Node node){
+        if(node==null){
+            return;
+        }
+        System.out.print(node.data);
+        pre(node.left);
+        pre(node.right);
+    }
+    public static void in(Node node){
+        if(node==null){
+            return;
+        }
+        in(node.left);
+        System.out.print(node.data);
+        in(node.right);
+    }
+    public static void post(Node node){
+        if(node==null){
+            return;
+        }
+        post(node.left);
+        post(node.right);
+        System.out.print(node.data);
+    }
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int n= scanner.nextInt();
+        scanner.nextLine();
+        for(int i=0; i<n; i++){
+            String str = scanner.nextLine();
+            insertNode(head,str.charAt(0),str.charAt(2),str.charAt(4));
+        }
+        pre(head);
+        System.out.println();
+        in(head);
+        System.out.println();
+        post(head);
+    }
+
+}
