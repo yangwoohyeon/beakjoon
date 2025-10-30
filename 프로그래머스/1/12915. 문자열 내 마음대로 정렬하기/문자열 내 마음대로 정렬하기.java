0f52765e84300ce969d48cfb5c8bdb8d0758e30b
yangwoohyeon <dngus0717@gmail.com>
2025-10-30 22:26:21 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.25 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -1,20 +1,18 @@
 class Solution {
     public String[] solution(String[] strings, int n) {
-        for(int i=strings.length-1; i>0; i--){
-            for(int j=0; j<i; j++){
-                if(strings[j].charAt(n)>strings[j+1].charAt(n)){
-                    String tmp = strings[j];
-                    strings[j]=strings[j+1];
-                    strings[j+1]=tmp;
+        for(int i=0; i<strings.length-1; i++){
+            int min = i;
+            for(int j=i+1; j<strings.length; j++){
+                if(strings[min].charAt(n)>strings[j].charAt(n)){
+                    min=j;
                 }
-                else if(strings[j].charAt(n)==strings[j+1].charAt(n)){
-                    if(strings[j].compareTo(strings[j+1])>0){
-                        String tmp = strings[j];
-                        strings[j]=strings[j+1];
-                        strings[j+1]=tmp;
-                    }
+                else if(strings[min].charAt(n)==strings[j].charAt(n) && strings[min].compareTo(strings[j])>0){
+                    min=j;
                 }
             }
+            String tmp = strings[min];
+            strings[min] = strings[i];
+            strings[i]=tmp;
         }
         return strings;
     }
