801eaba743fe8c0e098e81454b72c1ccdab7cdf2
yangwoohyeon <dngus0717@gmail.com>
2025-10-30 22:48:47 +0900

    [level 2] Title: 가장 큰 수, Time: 222.74 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,19 +1,18 @@
-import java.util.Arrays;
-
-public class Solution {
+import java.util.*;
+class Solution {
     public String solution(int[] numbers) {
-       String[] str = new String[numbers.length];
+        String[] str = new String[numbers.length];
         for(int i=0; i<numbers.length; i++){
-            str[i] = Integer.toString(numbers[i]);
+            str[i]=Integer.toString(numbers[i]);
         }
         Arrays.sort(str,(a,b)->(b+a).compareTo(a+b));
         if(str[0].equals("0")){
             return "0";
         }
-        StringBuilder answer = new StringBuilder();
-        for(String s : str){
-            answer.append(s);
+        StringBuilder s = new StringBuilder();
+        for(int i=0; i<numbers.length; i++){
+            s.append(str[i]);
         }
-        return answer.toString();
+        return s.toString();
     }
 }
