9402e178a29cb6946ccf78547d87cba1e5f181b3
yangwoohyeon <dngus0717@gmail.com>
2025-11-04 13:27:27 +0900

    [level 3] Title: 단어 변환, Time: 0.40 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,34 +1,41 @@
+import java.util.*;
 class Solution {
-    boolean[] visit;
-    int answer=0;
     public int solution(String begin, String target, String[] words) {
-        visit = new boolean[words.length];
-        dfs(begin,target,words,0);
-        return answer;
-    }
-    
-    public void dfs(String cur, String dst, String[] words, int count){
-        if(cur.equals(dst)){
-            answer=count;
-            return;
-        }
-        for(int i=0; i<words.length; i++){
-            if(!visit[i] && isConvertable(cur,words[i])){
-                visit[i]=true;
-                dfs(words[i],dst,words,count+1);
-                visit[i]=false;
+        Queue<String> queue = new LinkedList<>();
+        queue.add(begin);
+        int count = 0;
+        boolean[] visit = new boolean[words.length];
+        while(!queue.isEmpty()){
+            String str = queue.poll();
+            if(str.equals(target)){
+                break;
+            }
+            boolean checkCount = false;
+            for(int i=0; i<words.length; i++){
+                if(!visit[i] && check(str,words[i])){
+                    queue.add(words[i]);
+                    visit[i]=true;
+                    checkCount=true;
+                }
+            }
+            if(checkCount){
+                count++;
+            }
+            if(queue.isEmpty()){
+                return 0;
             }
         }
+        return count;
+        
     }
-    
-    public boolean isConvertable(String src, String dst){
-        int diff = 0;
-        for(int i=0; i<src.length(); i++){
-            if(src.charAt(i)!=dst.charAt(i)){
-                diff++;
+    public boolean check(String s1, String s2){
+        int count=0;
+        for(int i=0; i<s1.length(); i++){
+            if(s1.charAt(i)!=s2.charAt(i)){
+                count++;
             }
         }
-        if(diff==1){
+        if(count==1){
             return true;
         }
         return false;
