63130aff821e9a242db0df966e5ab926916fbfea
yangwoohyeon <dngus0717@gmail.com>
2025-11-04 14:44:52 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.69 ms, Memory: 54.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,86 +1,47 @@
 import java.util.*;
 class Solution {
     boolean[][] visit;
-    int answer=Integer.MAX_VALUE;
-    boolean check=false;
-    public int solution(int[][] maps) {
-        
-        visit = new boolean[maps.length][maps[0].length];
-        //dfs(maps,0,0,1);
 
-        
-        bfs(maps);
-        
-        
-        if(!check){
-            return -1;
-        }
-        return answer;
+    public int solution(int[][] maps) {
+        visit = new boolean[maps.length][maps[0].length];
+        return bfs(maps);
     }
-    public void bfs(int[][] maps){
+    public int bfs(int[][] maps){
         Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{0,0,1});
-        visit[0][0]=true;
+        queue.add(new int[]{0,0});
+        int count = 1;
         while(!queue.isEmpty()){
-            int[] pos = queue.poll();
-            int x = pos[0];
-            int y = pos[1];
-            int count = pos[2];
-            if(x==maps.length-1 && y==maps[0].length-1){
-                check=true;
-                answer=count;
-                break;
+            int size = queue.size();
+            for(int i=0; i<size; i++){
+                int[] pos= queue.poll();
+                int x=pos[0];
+                int y=pos[1];
+                if(x==maps.length-1 && y==maps[0].length-1){
+                    return count;
+                }
+                //위
+                if(x-1>=0 && !visit[x-1][y] && maps[x-1][y]==1){
+                    queue.add(new int[]{x-1,y});
+                    visit[x-1][y]=true;
+                }
+                //아래
+                if(x+1<maps.length && !visit[x+1][y] && maps[x+1][y]==1){
+                    queue.add(new int[]{x+1,y});
+                    visit[x+1][y]=true;
+                }
+                //오른쪽
+                if(y+1<maps[0].length && !visit[x][y+1] && maps[x][y+1]==1){
+                    queue.add(new int[]{x,y+1});
+                    visit[x][y+1]=true;
+                }
+                //왼쪽
+                if(y-1>=0 && !visit[x][y-1] && maps[x][y-1]==1){
+                    queue.add(new int[]{x,y-1});
+                    visit[x][y-1]=true;
+                }
             }
-            //아래로 한칸
-        if(x+1 <maps.length && maps[x+1][y]==1 && !visit[x+1][y]){
-            visit[x+1][y]=true;
-            queue.offer(new int[]{x+1,y,count+1});
+            count++;
         }
-        //오른쪽으로 이동
-        if(y+1 <maps[0].length && maps[x][y+1]==1 &&!visit[x][y+1]){
-            visit[x][y+1]=true;
-            queue.offer(new int[]{x,y+1,count+1});
-        }
-        //왼쪽으로 이동
-        if(y-1>=0 && maps[x][y-1]==1&&!visit[x][y-1]){
-            visit[x][y-1]=true;
-            queue.offer(new int[]{x,y-1,count+1});
-        }
-        //위로 이동
-        if(x-1 >=0 && maps[x-1][y]==1&&!visit[x-1][y]){
-            visit[x-1][y]=true;
-            queue.offer(new int[]{x-1,y,count+1});
-        }
-        }
-    }
-    public void dfs(int[][] maps, int x, int y, int count){
-        visit[x][y]=true;
-        if(x==maps.length-1 && y==maps[0].length-1){
-            check=true;
-            if(count<answer){
-                answer=count;
-            }
-            visit[x][y]=false;
-            return;
-        }
-    
-        //아래로 한칸
-        if(x+1 <maps.length && maps[x+1][y]==1 && !visit[x+1][y]){
-            dfs(maps,x+1,y,count+1);
-        }
-        //오른쪽으로 이동
-        if(y+1 <maps[0].length && maps[x][y+1]==1 &&!visit[x][y+1]){
-            dfs(maps,x,y+1,count+1);
-        }
-        //왼쪽으로 이동
-        if(y-1>=0 && maps[x][y-1]==1&&!visit[x][y-1]){
-            dfs(maps,x,y-1,count+1);
-        }
-        //위로 이동
-        if(x-1 >=0 && maps[x-1][y]==1&&!visit[x-1][y]){
-            dfs(maps,x-1,y,count+1);
-        }
-        visit[x][y]=false;
-        
+        return -1;
     }
 }
