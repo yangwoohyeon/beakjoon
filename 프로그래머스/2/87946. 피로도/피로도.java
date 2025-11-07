c5be479c6767afd6de3cb6ef11862e641e7a0e6e
yangwoohyeon <dngus0717@gmail.com>
2025-11-07 22:12:09 +0900

    [level 2] Title: 피로도, Time: 3.10 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,29 +1,27 @@
-import java.util.*;
-
 class Solution {
-    int answer;
+    int ans = Integer.MIN_VALUE;
+    boolean[] visit;
     public int solution(int k, int[][] dungeons) {
-       boolean[] visit = new boolean[dungeons.length];
-        answer = Integer.MIN_VALUE;
-        dfs(k,dungeons,visit,0);
-        return answer;
-        
+        visit = new boolean[dungeons.length];
+        dfs(k,0,dungeons);
+        return ans;
     }
-    public void dfs(int k, int[][] dungeons, boolean[] visit, int count){
-        for(int i=0; i<dungeons.length; i++){
-            if(visit[i]==false && k>=dungeons[i][0]){
-                visit[i]=true;
-                count++;
-                if(count>answer){
-                    answer = count;
-                }
-                dfs(k-dungeons[i][1],dungeons,visit,count);
-                visit[i]=false;
-                count--;
-            }
-            
+    private void dfs(int k, int count, int[][] dungeons){
+        if(k<0){
+            if(count>ans){
+            ans=count;
         }
-        
+            return;
+        }
+        for(int i=0; i<dungeons.length; i++){
+            if(!visit[i] && k>=dungeons[i][0]){
+                visit[i]=true;
+                dfs(k-dungeons[i][1], count+1, dungeons);
+                visit[i]=false;
+            }
+        }
+        if(count>ans){
+            ans=count;
+        }
     }
- 
-} 
+}
