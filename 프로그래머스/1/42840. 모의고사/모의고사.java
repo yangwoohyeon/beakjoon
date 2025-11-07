a3cbbe41b9ac96be2a0ed2c2ed04f6afd5669da8
yangwoohyeon <dngus0717@gmail.com>
2025-11-07 21:38:24 +0900

    [level 1] Title: 모의고사, Time: 1.18 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,43 +2,54 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] student1 = {1,2,3,4,5};
-        int[] student2 = {2,1,2,3,2,4,2,5};
-        int[] student3 = {3,3,1,1,2,2,4,4,5,5};
-        int a1 = check(student1,answers);
-        int a2 = check(student2,answers);
-        int a3 = check(student3,answers);
-        int max  = Math.max(a1,Math.max(a2,a3));
-        List<Integer> list = new ArrayList<>();
-        if(a1==max){
-            list.add(1);
-        }
-        if(a2==max){
-            list.add(2);
-        }
-        if(a3==max){
-            list.add(3);
-        }
-        int[] arr= new int[list.size()];
-        for(int i=0; i<list.size(); i++){
-            arr[i]=list.get(i);
-        }
-        return arr;
-    }
-    
-    public int check(int[] student, int[] answers ){
-        int size = student.length;
-        int count=0;
-        int pos=0;
+        int[] a = {1,2,3,4,5};
+        int[] b = {2,1,2,3,2,4,2,5};
+        int[] c = {3,3,1,1,2,2,4,4,5,5};
+        
+        int[] ans = new int[3];
+        int a_idx = 0;
+        int b_idx = 0;
+        int c_idx = 0;
         for(int i=0; i<answers.length; i++){
-            if(answers[i]==student[pos]){
-                count++;
+            if(a[a_idx]==answers[i]){
+                ans[0]++;
             }
-            pos++;
-            if(pos>=size){
-                pos=0;
+            if(b[b_idx]==answers[i]){
+                ans[1]++;
+            }
+            if(c[c_idx]==answers[i]){
+                ans[2]++;
+            }
+            a_idx++;
+            b_idx++;
+            c_idx++;
+            if(a_idx>=a.length){
+                a_idx=0;
+            }
+            if(b_idx>=b.length){
+                b_idx=0;
+            }
+            if(c_idx>=c.length){
+                c_idx=0;
             }
         }
-        return count;
+        int max = ans[0];
+        for(int i=0; i<3; i++){
+            if(ans[i]>max){
+                max=ans[i];
+            }
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=0; i<3; i++){
+            if(max==ans[i]){
+                queue.add(i+1);
+            }
+        }
+        int size=queue.size();
+        int[] answer = new int[size];
+        for(int i=0; i<size; i++){
+            answer[i]=queue.poll();
+        }
+        return answer;
     }
 }
