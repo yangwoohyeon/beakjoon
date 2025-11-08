93582d39656f7348bbb392dd4a85f22b5faec521
yangwoohyeon <dngus0717@gmail.com>
2025-11-08 23:12:56 +0900

    [level 2] Title: 모음 사전, Time: 25.83 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,28 +1,22 @@
-import java.util.*;
-
 class Solution {
-    char[] CHARS={'A','E','I','O','U'};
+    char[] CHAR = {'A', 'E', 'I', 'O', 'U'};
     int count = 0;
-    int ans = 0;
-    int answer = 0;
+    int answer=0;
     public int solution(String word) {
         dfs("",word);
-        return ans;
+        return answer;
         
     }
     public void dfs(String s, String word){
-        if(word.equals(s)){
-            answer = 1;
-            ans=count;
+        if(s.equals(word)){
+            answer=count;
+            return;
         }
-        if(answer == 0 && s.length() != 5){
-            for(char c : CHARS){
+        for(char c : CHAR){
+            if((s+c).length() <= 5){
                 count++;
                 dfs(s+c,word);
             }
         }
-        if(answer == 1){
-            return;
-        }
-    } 
+    }
 }
