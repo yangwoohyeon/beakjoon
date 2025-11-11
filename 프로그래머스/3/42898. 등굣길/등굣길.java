cec06685fdb6eb3798d4fd19d8ea33dcb1844421
yangwoohyeon <dngus0717@gmail.com>
2025-11-11 23:14:29 +0900

    [level 3] Title: 등굣길, Time: 1.88 ms, Memory: 54.3 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,12 +1,9 @@
 import java.util.*;
-
 class Solution {
-    static final int MOD = 1_000_000_007;
-    int[][] mem; // -1: 웅덩이, 0: 아직 미계산, >0: 계산된 경로 수
-    int N, M;
-
+    int[][] mem;
+    int N,M;
     public int solution(int m, int n, int[][] puddles) {
-        N = n;
+         N = n;
         M = m;
         mem = new int[N][M];
         for (int[] p : puddles) {
@@ -15,22 +12,20 @@ class Solution {
             mem[y][x] = -1; // 웅덩이
         }
         return dfs(0, 0);
+        
     }
-
-    private int dfs(int x, int y) {
+    public int dfs(int x, int y){
         // 범위 밖
         if (x >= N || y >= M) return 0;
         // 웅덩이
         if (mem[x][y] == -1) return 0;
         // 도착
         if (x == N - 1 && y == M - 1) return 1;
-        // 이미 계산한 경로 수 있으면 재사용
         if (mem[x][y] > 0) return mem[x][y];
-
-        long down = dfs(x + 1, y);
-        long right = dfs(x, y + 1);
-
-        mem[x][y] = (int)((down + right) % MOD);
+        long down = dfs(x+1,y);
+        long right = dfs(x,y+1);
+        
+        mem[x][y] = (int)((down + right)%1000000007);
         return mem[x][y];
     }
 }
