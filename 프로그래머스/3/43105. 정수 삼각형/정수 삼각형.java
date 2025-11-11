0039fc30171e77bd7a10976270f82378ff0f0749
yangwoohyeon <dngus0717@gmail.com>
2025-11-11 22:07:12 +0900

    [level 3] Title: 정수 삼각형, Time: 11.28 ms, Memory: 63.1 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,29 +1,30 @@
 import java.util.*;
-
 class Solution {
     int[][] mem;
     public int solution(int[][] triangle) {
-        mem = new int[triangle.length][triangle.length];
-        for(int i=0; i<mem.length; i++){
+        mem = new int[triangle.length][triangle[triangle.length-1].length];
+        for(int i=0; i < triangle.length; i++){
             Arrays.fill(mem[i],-1);
         }
-        mem[0][0]=triangle[0][0];
-        for(int i=0; i<triangle.length-1; i++){
-            down(triangle,i);
-        }
-        int answer = Integer.MIN_VALUE;
-        for(int i=0; i<triangle[triangle.length-1].length; i++){
-            if(answer<mem[triangle.length-1][i]){
-                answer = mem[triangle.length-1][i];
-            }
-        }
-        return answer;
+
+        return dfs(triangle,0,0);
+       
+        
     }
-    private void down(int[][] triangle, int hight){ //hight:현재 높이
-        int width = triangle[hight].length; //현재 높이의 넓이
-        for(int i=0; i<width; i++){
-            mem[hight+1][i]=Math.max(mem[hight][i]+triangle[hight+1][i],mem[hight+1][i]); //대각선 왼쪽
-            mem[hight+1][i+1]=Math.max(mem[hight][i]+triangle[hight+1][i+1],mem[hight+1][i+1]);//대각선 오른쪽
+    private int dfs(int[][] triangle, int x, int y){
+        
+        if(x>= triangle.length || y >= triangle[x].length){
+            return 0;
         }
+        if(mem[x][y]!=-1){
+            return mem[x][y];
+        }
+ 
+        int down = dfs(triangle,x+1,y); //바로 아래로 이동
+        int diag = dfs(triangle,x+1,y+1); //대각선 오른쪽 아래 이동
+        
+        mem[x][y] = triangle[x][y] + Math.max(down,diag);
+        return mem[x][y];
+        
     }
 }
