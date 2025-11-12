2d72bc9dcada7a7b7df02afa1ac0f4983cf71908
yangwoohyeon <dngus0717@gmail.com>
2025-11-12 22:18:34 +0900

    [level 2] Title: 하노이의 탑, Time: 5.46 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/README.md
+++ 프로그래머스/2/12946. 하노이의 탑/README.md
@@ -1,10 +1,10 @@
 # [level 2] 하노이의 탑 - 12946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12946?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12946) 
 
 ### 성능 요약
 
-메모리: 127 MB, 시간: 9.62 ms
+메모리: 114 MB, 시간: 5.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 14일 18:35:04
+2025년 11월 12일 22:18:32
 
 ### 문제 설명
 
--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -1,20 +1,22 @@
 import java.util.*;
-
 class Solution {
+    Queue<int[]> queue = new LinkedList<>();
     public int[][] solution(int n) {
-
-        return hanoi(n,1,3).toArray(new int[0][1]);
-    }
-    
-    private List<int[]> hanoi(int n, int from, int to){
-        if(n == 1){
-            return List.of(new int[]{from,to});
+        hanoi(n,1,2,3);
+        int size = queue.size();
+        int[][] answer= new int[size][2];
+        for(int i=0; i<size; i++){
+            answer[i]=queue.poll();
         }
-        int empty = 6-from-to;
-        List<int[]> result = new ArrayList<>();
-        result.addAll(hanoi(n-1,from,empty));
-        result.addAll(hanoi(1,from,to));
-        result.addAll(hanoi(n-1,empty,to));
-        return result;
+        return answer;
+    }
+    public void hanoi(int n, int h1, int h2, int h3){
+        if(n==1){
+            queue.add(new int[] {h1,h3});
+            return;
+        }
+        hanoi(n-1,h1,h3,h2);
+        queue.add(new int[] {h1,h3});
+        hanoi(n-1,h2,h1,h3);
     }
 }
