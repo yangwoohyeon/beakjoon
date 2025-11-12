99687f9b0a6231400e60e596a627be32303f0869
yangwoohyeon <dngus0717@gmail.com>
2025-11-12 21:46:31 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 10.40 ms, Memory: 135 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,30 +1,36 @@
 class Solution {
-    static int[] answer= new int[2];
+    int[] ans = new int[2];
     public int[] solution(int[][] arr) {
         dp(arr,0,0,arr.length);
-        
-        return answer;
+        return ans;
     }
-    public boolean check(int[][] arr, int x, int y,int n){
-        for(int i=x; i<x+n; i++){
-            for(int j=y; j<y+n; j++){
-                if(arr[x][y]!=arr[i][j]){
-                    return false;
+    public boolean check(int[][] arr, int x, int y, int n){
+        if(x+n<=arr.length && y+n<=arr.length){
+        int a = arr[x][y];
+            for(int i=x; i<x+n; i++){
+                for(int j=y; j<y+n; j++){
+                    if(a!=arr[i][j]){
+                        return false;
+                    }
                 }
             }
+            return true;
         }
-        return true;
+        return false;
     }
-    public void dp(int[][] arr, int x, int y,int n){
-        if(check(arr,x,y,n)){
-            answer[arr[x][y]]++;
+    public void dp(int[][] arr,int x,int y, int size){
+        if(x>=arr.length || y>=arr.length){
             return;
         }
-        n=n/2;
-        dp(arr,x,y,n);
-        dp(arr,x+n,y,n);
-        dp(arr,x,y+n,n);
-        dp(arr,x+n,y+n,n);
+        if(check(arr,x,y,size)){
+            ans[arr[x][y]]++;
+            return;
+        }
+
+        size=size/2;
+        dp(arr,x,y,size);
+        dp(arr,x+size,y,size);
+        dp(arr,x,y+size,size);
+        dp(arr,x+size,y+size,size);
     }
-    
 }
