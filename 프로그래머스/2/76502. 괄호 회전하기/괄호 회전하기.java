d35da2fb24f0379a7e06ddb75c248924dbb14709
yangwoohyeon <dngus0717@gmail.com>
2025-11-14 23:36:58 +0900

    [level 2] Title: 괄호 회전하기, Time: 71.24 ms, Memory: 94.1 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,58 +1,68 @@
 import java.util.*;
 class Solution {
+    int count=0;
     public int solution(String s) {
-        int answer=0;
-        for(int i=0; i<s.length(); i++){
-            String str = rolling(s,i);
-            if(check(str)){
-                answer++;
-            }
-        }
-        return answer;
+        dfs(s,0);
+        return count;
     }
+    
+    public void dfs(String s,int n){
+       if(n>=s.length()){
+           return;
+       }
+        if(check(s)){
+            count++;
+        }
+        int size = s.length();
+        dfs(s.substring(1,size)+s.charAt(0),n+1);
+        
+    }
+    
+    
     public boolean check(String s){
         Stack<Character> stack = new Stack<>();
-        for(int i=0; i<s.length(); i++){
-            char c = s.charAt(i);
-            if(c=='[' || c=='(' || c=='{'){
-                stack.push(c);
+        int idx=0;
+        stack.push(s.charAt(idx));
+        idx++;
+        while(true){
+            if(idx>=s.length()){
+                break;
             }
-            else{
-                if(stack.isEmpty()){
+            char c = s.charAt(idx);
+            if(c=='(' || c=='{' || c=='['){
+                stack.push(c);
+                idx++;
+            }
+            else if(stack.isEmpty()){
+                return false;
+            }
+            else if(c==')'){
+                char a = stack.pop();
+                if(a!='('){
                     return false;
                 }
-                if(c=='}'){
-                    if(stack.pop()!='{'){
-                        return false;
-                    }
-                }
-                else if(c==']'){
-                    if(stack.pop()!='['){
-                        return false;
-                    }
-                }
-                else if(c==')'){
-                    if(stack.pop()!='('){
-                        return false;
-                    }
-                }
+                idx++;
             }
+            else if(c=='}'){
+                char a = stack.pop();
+                if(a!='{'){
+                    return false;
+                }
+                idx++;
+            }
+            else if(c==']'){
+                char a = stack.pop();
+                if(a!='['){
+                    return false;
+                }
+                idx++;
+            }
+            
         }
         if(!stack.isEmpty()){
             return false;
         }
         return true;
     }
-    
-    public String rolling(String s, int n){
-        StringBuilder str = new StringBuilder();
-        for(int i=0; i<s.length(); i++){
-            str.append(s.charAt(n));
-            n++;
-            if(n==s.length()){
-                n=0;
-            }
-        }
-        return str.toString();
-    }
+               
 }
